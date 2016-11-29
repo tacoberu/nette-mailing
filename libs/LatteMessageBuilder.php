7065b0a6e6df7bf063f5b2b216d837f31520c812
Martin Takáč <martin@takac.name>
2016-11-29 04:53:54 +0100

    LatteMessageBuilder, BuilderFactory

---
+++ libs/LatteMessageBuilder.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Copyright (c) since 2004 Martin Takáč (http://martin.takac.name)
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+
+namespace Taco\Nette\Mailing;
+
+use Latte\Loaders\StringLoader;
+use Nette\Mail\Message,
+	Nette\Utils\Validators,
+	Nette\Application\LinkGenerator,
+	Nette\Bridges\ApplicationLatte\ILatteFactory,
+	Nette\Bridges\ApplicationLatte\UIMacros;
+
+
+/**
+ * Generování mailu pomocí Latte šablony.
+ * @author Martin Takáč <martin@takac.name>
+ */
+class LatteMessageBuilder implements MessageBuilder
+{
+
+	/**
+	 * @var LinkGenerator
+	 */
+	private $linkGenerator;
+
+	/**
+	 * @var ILatteFactory
+	 */
+	private $latteFactory;
+
+	/**
+	 * @var array
+	 */
+	private $defaults = [];
+
+
+	function __construct(LinkGenerator $generator, ILatteFactory $latteFactory)
+	{
+		$this->linkGenerator = $generator;
+		$this->latteFactory = $latteFactory;
+	}
+
+
+
+	/**
+	 * @param string $name Name of value, which replaced in template.
+	 * @param mixin $value Value for template.
+	 */
+	function setOption($name, $value)
+	{
+		$this->defaults[$name] = $value;
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string $from Email of sender.
+	 * @param string $recipient Email of recipient.
+	 * @param MailContent $content
+	 * @param hashtable of string $values
+	 * @return Message
+	 */
+	function compose($from, $recipient, MailContent $content, array $values = [])
+	{
+		Validators::assert($from, 'email');
+		Validators::assert($recipient, 'email');
+
+		$args = array_merge($this->defaults, $values, ['_control' => $this->linkGenerator]);
+		$latte = $this->getLatte();
+
+		$mail = new Message;
+		$mail->setFrom($from)
+			->setSubject($latte->renderToString($content->getSubject(), $args));
+
+		if ($content->getBody()) {
+			$mail->setBody($latte->renderToString($content->getBody(), $args));
+		}
+
+		if ($content->getHtml()) {
+			$mail->setHtmlBody($latte->renderToString($content->getHtml(), $args));
+		}
+
+		$mail->addTo($recipient);
+
+		return $mail;
+	}
+
+
+
+	/**
+	 * @return Latte\Engine
+	 */
+	private function getLatte()
+	{
+		$latte = $this->latteFactory->create();
+		$latte->setLoader(new StringLoader);
+		UIMacros::install($latte->getCompiler());
+
+		return $latte;
+	}
+
+}
